a061e9981d594e3b0bfd7e6a105a7e0d77ab8277
Xuheng Liu <liuxuheng1990@gmail.com>
2021-08-24 20:23:50 +0800

    feat: fullfil

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 // ref:
 // - https://umijs.org/plugins/api
 import { IApi } from '@umijs/types';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
 
 export default function(api: IApi) {
-  api.logger.info('use plugin');
-
-  api.modifyHTML($ => {
-    $('body').prepend(`<h1>hello umi plugin</h1>`);
-    return $;
+  api.onGenerateFiles(() => {
+    const path =
+      api.env === 'production'
+        ? './src/.umi-production/umi.ts'
+        : './src/.umi/umi.ts';
+    const buffer = readFileSync(resolve(path));
+    const c = String(buffer);
+    api.writeTmpFile({
+      path: 'bootstrap.ts',
+      content: c,
+    });
+    api.writeTmpFile({
+      path: 'umi.ts',
+      content: 'import("./bootstrap")',
+    });
   });
 }
